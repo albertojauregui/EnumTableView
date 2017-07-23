f267c2f7d2196d72e599ac0107d97c62f30ff3aa
albertojauregui <bastertwo@gmail.com>
2017-07-23 14:56:45 -0600

    Added the enum TableSection and the data tracker

--- EnumTableView/ViewController.swift
+++ EnumTableView/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum TableSection: Int {
+        case action = 0, comedy, drama, indie, total
+    }
+    
+    let SectionHeaderHeight: CGFloat = 25
+    
+    var data = [TableSection: [[String: String]]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
